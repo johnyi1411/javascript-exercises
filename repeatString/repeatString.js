6ca178910504750d441c5ba13b9ad6b013db22f8
John Yi <john.yi1411@gmail.com>
2019-06-13 20:05:58 -0500

    completed helloWorld, repeatString and reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  let output = '';
+  if (number >= 0) {
+    for (let i = 0; i < number; i++) {
+        output += string;
+    }
+    return output;
+  } else {
+    return 'ERROR';
+  }
 }
 
 module.exports = repeatString
